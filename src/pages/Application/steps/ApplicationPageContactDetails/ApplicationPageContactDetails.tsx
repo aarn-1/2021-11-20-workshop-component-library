78cdbdc9a3977ef2cd12f50b6b253a07fdf59bb7
Aaron Gnan <aaron.gnan@student.kit.edu>
2021-11-20 17:09:47 +0100

    Add Components to ApplicationPageContactDetails.tsx

--- src/pages/Application/steps/ApplicationPageContactDetails/ApplicationPageContactDetails.tsx
+++ src/pages/Application/steps/ApplicationPageContactDetails/ApplicationPageContactDetails.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Button } from "../../../../components/Button/Button";
+import { InputField, InputType } from "../../../../components/InputField/InputField";
+import { Select } from "../../../../components/Select/Select";
 import { ApplicationPageStep } from "../../components/ApplicationPageStep";
 
 interface ApplicationPageContactDetailsProps {
@@ -21,7 +24,19 @@ export const ApplicationPageContactDetails = ({
       showPreviousPageButton={false}
       handleGoToPreviousStep={handleGoToPreviousStep}
     >
-      <div></div>
+      <div>
+        label?: string,
+        value?: string,
+        validation: boolean,
+        validationText: string,
+        placeholderText: string,
+        type: InputType,
+        <InputField placeholderText={'placeholder'} type={InputType.text}
+        validation={false} validationText={'validation text'} label={'label'}
+        ></InputField>
+        <h2>Staatsangehörigkeit</h2>
+        <Select options={['DE','EN','FR']} placeholder={'select something'}></Select>
+      </div>
     </ApplicationPageStep>
   );
 };
